cdf5929b1a2f0ebb88ac6d9ddf546838aad7ad43
LeonnMartins <leandson.leon@gmail.com>
2023-11-23 16:53:22 -0300

    Atualização

--- script.js
+++ script.js
@@ -20,22 +20,22 @@ function displayImage(bmi) {
     var imageElement = document.getElementById("image");
 
     if (bmi < 18.5) {
-        imageElement.src = "./imagens/magro.png";
+        imageElement.src = "../imagens/magro.png";
         imageElement.alt = "Peso insuficiente"
     } else if (bmi < 24.9) {
-        imageElement.src = "./imagens/normal.png";
+        imageElement.src = "../imagens/normal.png";
         imageElement.alt ="Normopeso"
     } else if (bmi < 29.9) {
-        imageElement.src = "./imagens/sobrepeso.png";
+        imageElement.src = "../imagens/sobrepeso.png";
         imageElement.alt ="Sobrepeso"
     } else if (bmi < 34.9) {
-        imageElement.src = "./imagens/obesidade.png";
+        imageElement.src = "../imagens/obesidade.png";
         imageElement.alt ="Obesidade"
     } else if (bmi < 39.9) {
-        imageElement.src = "./imagens/obesidade mórbida.jpg";
+        imageElement.src = "../imagens/obesidade mórbida.jpg";
         imageElement.alt ="Obesidade mórbida"
     } else {
-        imageElement.src = "./imagens/obesidade mórbida.jpg";
+        imageElement.src = "../imagens/obesidade mórbida.jpg";
         imageElement.alt ="Obesidade mórbida"
     }
 }
